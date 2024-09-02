bcafb7052de38bae9450984b3f57786d607c3640
tatianacardosolima <tatidornel@gmail.com>
2024-09-02 00:45:48 -0300

    Add Masstransit e RabbitMQ

---
+++ src/04-Infrastructure/Publications/SaveContactPublisher.cs
@@ -0,0 +1,18 @@
+﻿using PosTech.Fase3.AddContact.Domain.Interfaces;
+using PosTech.Fase3.AddContact.Domain.Requests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PosTech.Fase3.AddContact.Infrastructure.Publications
+{
+    public class SaveContactPublisher : ISaveContactPublisher
+    {
+        public Task<bool> PublishAsync(NewContactRequest request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
