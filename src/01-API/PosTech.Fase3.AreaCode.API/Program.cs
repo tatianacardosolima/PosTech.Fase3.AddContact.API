3ce02295bee1efff5d6ae700ee31d6730b2c557c
tatianacardosolima <tatidornel@gmail.com>
2024-09-25 21:51:55 -0300

    Ajustado a porta

--- src/01-API/PosTech.Fase3.AreaCode.API/Program.cs
+++ src/01-API/PosTech.Fase3.AreaCode.API/Program.cs
@@ -1,20 +1,22 @@
 var builder = WebApplication.CreateBuilder(args);
 
+
+builder.WebHost.UseUrls("http://*:8081");
 // Add services to the container.
-//builder.WebHost.UseUrls("http://*:8081");
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
 
+var app = builder.Build();
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseHttpsRedirection();
 
